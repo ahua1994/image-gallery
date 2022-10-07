a51e49d3a0ab62f430149a7a36112d92d4cb3645
Andy Hua <user.andy.hua@gmail.com>
2022-10-07 14:35:32 -0700

    slightly better

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,16 +2,16 @@ import data from "./AlbumData";
 import "./Album.css";
 
 function Album() {
-    const albums = [];
-    data.forEach((album) =>
-        albums.push(
-            <figure className="col-xs-12 col-sm-6 col-md-3">
-                <img className="col-12" src={album.download_url}></img>
-                <figcaption>{album.author}</figcaption>
-            </figure>
-        )
+    return (
+        <>
+            {data.map((album) => (
+                <figure className="col-xs-12 col-sm-6 col-md-3">
+                    <img className="col-12" src={album.download_url}></img>
+                    <figcaption>{album.author}</figcaption>
+                </figure>
+            ))}
+        </>
     );
-    return <>{albums}</>;
 }
 
 export default Album;
